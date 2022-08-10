6814b42ad5da2e23f8d635d6c263e41e5a747dc6
Emira <emiradga888@gmail.com>
2022-08-10 13:45:03 +0100

    deleting some files that i dont need anymore

--- src/main/java/com/wct/animall/controller/AnimalController.java
+++ src/main/java/com/wct/animall/controller/AnimalController.java
@@ -8,5 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/")
 public class AnimalController {
-
+	/*
+	 * @Autowired private EmployeeRepository employeeRepository;
+	 * 
+	 * // get all employees
+	 * 
+	 * @GetMapping("/employees") public List<Employee> getAllEmployees() { return
+	 * employeeRepository.findAll(); }
+	 */
 }
