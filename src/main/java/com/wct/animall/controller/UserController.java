136df551068a071d873d7e2a52a9f24fd53024e3
Emira <emiradga888@gmail.com>
2022-08-11 16:05:39 +0100

    creation of UserController

--- src/main/java/com/wct/animall/controller/UserController.java
+++ src/main/java/com/wct/animall/controller/UserController.java
@@ -1,5 +1,28 @@
 package com.wct.animall.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wct.animall.model.User;
+import com.wct.animall.repository.UserRepository;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/v1/")
 public class UserController {
 
+	@Autowired
+	private UserRepository userRepository;
+
+	// get all users
+
+	@GetMapping("/user")
+	public List<User> getAllAnimals() {
+		return userRepository.findAll();
+	}
 }
