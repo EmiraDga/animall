5b0dd195b083d21df2590971233c8aa053dfad88
Emira <emiradga888@gmail.com>
2022-08-28 12:58:38 +0100

    create the authority class of User roles

--- src/main/java/com/wct/animall/model/Authority.java
+++ src/main/java/com/wct/animall/model/Authority.java
@@ -1,5 +1,16 @@
 package com.wct.animall.model;
 
 public enum Authority {
+<<<<<<< HEAD
 	ADMIN, USER
+=======
+	USER("USER"), ADMIN("ADMIN");
+
+	private String name;
+
+	private Authority(String name) {
+		this.name = name;
+	}
+
+>>>>>>> 9827279 (create the authority class of User roles)
 }
